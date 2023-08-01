0d939ac009c66b1658ef97247df45ebb2180676b
Sandeep <sandy6862g22@gmail.com>
2023-08-01 18:00:25 +0530

    created the users screen to show people

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Sidebar } from '../../components'
+import getUsers from '../../actions/getUsers'
+import UserList from './components/UserList'
 
 const UsersLayout = async({children}:{children: React.ReactNode}) => {
+  const users = await getUsers()
   return (
     <div className='h-full'>
         <Sidebar>
+          <UserList items={users} />
             {children}
         </Sidebar>
     </div>
