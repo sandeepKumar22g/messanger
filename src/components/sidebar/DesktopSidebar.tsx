f5d4fc482012d6db2fd506ec763ae92db21390f2
Sandeep <sandy6862g22@gmail.com>
2023-08-01 16:25:40 +0530

    added avatar and getusers

--- src/components/sidebar/DesktopSidebar.tsx
+++ src/components/sidebar/DesktopSidebar.tsx
@@ -3,10 +3,21 @@
 import React, {useState} from 'react'
 import useRoutes from '../../hooks/useRoutes'
 import DesktopItem from './DesktopItem'
+import Avatar from '../Avatar'
+import { User } from '@prisma/client'
 
+
+// interface DesktopSidebarProps {
+//   currentUser: User
+//   }
+
+// const DesktopSidebar: React.FC<DesktopSidebarProps> = ({currentUser}) => {
 const DesktopSidebar = () => {
   const routes = useRoutes()
   const [isOpen, setIsOpen] = useState(false)
+
+  // console.log(currentUser, "user")
+
   return (
     <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between'>
       <nav className='mt-4 flex flex-col justify-between'>
@@ -15,6 +26,12 @@ const DesktopSidebar = () => {
             <DesktopItem key={item.label} href={item.href} label={item.label} icon={item.icon} active={item.active} onClick={item.onClick} />
           ))}
         </ul>
+      </nav>
+      <nav className='mt-4 flex flex-col justify-between items-center'>
+        <div onClick={()=>setIsOpen(true)} className='cursor-pointer hover:opacity-75 transition' >
+          {/* <Avatar user={currentUser} /> */}
+          <Avatar  />
+        </div>
       </nav>
     </div>
   )
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import DesktopSidebar from './DesktopSidebar'
 import MobileFooter from './MobileFooter'
+import getCurrentUser from '../../actions/getCurrentUser'
 
 const Sidebar = async({children}: {children:React.ReactNode}) => {
+  // const currentUser = await getCurrentUser()
+  // console.log(c)
+
   return (
     <div className='h-full'>
-        <DesktopSidebar />
+        {/* <DesktopSidebar currentUser={currentUser} /> */}
+        <DesktopSidebar  />
         <MobileFooter />
         <main className='lg:pl-20 h-full'>
         {children}
